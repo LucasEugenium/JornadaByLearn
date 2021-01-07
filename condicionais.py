5b67d18a7c9cbcda56f0e9ecf9f093ce9cd5632e
LucasEugenium <74687374+LucasEugenium@users.noreply.github.com>
2021-01-06 22:10:30 -0300

    Add files via upload

---
+++ condicionais.py
@@ -0,0 +1,8 @@
+# condicionais
+temperatura = input('Digite se esta quente ou frio ')
+if temperatura == 'quente':
+  print('vamos tomar sorvete ')
+  print('vamos para praia')
+else:
+  print('vamos tomar chocolate')
+  print('vamos para o cinema')
